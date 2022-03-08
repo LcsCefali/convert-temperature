51f3dc62ed22e6ce53f4316b0e7cde91d70342f8
LcsCefali <lucas.cefali@gmail.com>
2022-03-08 17:54:28 -0300

    feat: added list view

--- app/src/main/java/com/example/converttemperature/MainActivity.java
+++ app/src/main/java/com/example/converttemperature/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.converttemperature;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<String> history = new ArrayList(Arrays.asList("Alex", "Fran"));
+
+        ListView historyList = findViewById(R.id.historyList);
+        historyList.setAdapter(
+                new ArrayAdapter<String>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        history
+                )
+        );
     }
 }
